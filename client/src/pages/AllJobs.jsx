b12c01850275788693639d4f2cc8f15fc0a69037
Weston Kostura <weston.kostura@gmail.com>
2023-11-20 21:17:35 -0700

    pages and components

---
+++ client/src/pages/AllJobs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './AllJobs.css';
+
+import JobList from '../components/JobList';
+
+const AllJobs = () => {
+    return (
+        <div>
+            <h1 className="featured-jobs-title">All Featured Jobs</h1>
+            <JobList />
+        </div>
+    );
+}
+
+export default AllJobs;
---
+++ client/src/pages/SingleJobPage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import jobs from '../../../server/seeders/jobSeeds.json';
+import ApplyForm from '../components/ApplyForm';
+import JobDetails from '../components/JobDetails';
+import './SingleJobPage.css';
+
+const SingleJobPage = () => {
+    let { title } = useParams();
+
+    // Find the job with the matching title
+    const job = jobs.find(job => job.jobTitle === title);
+
+    // If no job is found, return a not found message
+    if (!job) {
+        return <p>Job not found</p>;
+    }
+
+    return (
+        <div className="singleJobPage">
+            <div className="jobTitle">
+                <h1>{job.jobTitle}</h1>
+            </div>
+            <div className="applyForm">
+                <h2>Apply for this job</h2>
+                <ApplyForm job={job} />
+            </div>
+            <div className="jobDetails">
+                <JobDetails job={job} />
+            </div>
+        </div>
+    );
+};
+
+export default SingleJobPage;
