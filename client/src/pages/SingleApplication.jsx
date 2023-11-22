bc366da8c5d77f3a2771a374f4852f36f7355143
Weston Kostura <weston.kostura@gmail.com>
2023-11-21 21:08:00 -0700

    filter and all applications list working with database

--- client/src/pages/SingleApplication.jsx
+++ client/src/pages/SingleApplication.jsx
@@ -1,31 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import candidates from '../../../server/seeders/candidateSeeds.json';
+import { useQuery, gql } from '@apollo/client'; // new import
+
+const GET_CANDIDATE = gql`
+    query GetCandidate($id: ID!) {
+        candidate(id: $id) {
+            id
+            firstName
+            lastName
+            phoneNumber
+            email
+            job {
+                jobTitle
+            }
+        }
+    }
+`;
 
 const SingleApplication = () => {
-    const { firstName } = useParams();
-    const [candidate, setCandidate] = useState(null);
+    const { id } = useParams(); // change firstName to id
+    const { loading, error, data } = useQuery(GET_CANDIDATE, {
+        variables: { id },
+    });
 
-    useEffect(() => {
-        const foundCandidate = candidates.find(candidate => candidate.firstName.toLowerCase() === firstName);
-        setCandidate(foundCandidate);
-    }, [firstName]);
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error :(</div>;
 
-    if (!candidate) {
-        return <div>Loading...</div>;
-    }
+    const candidate = data.candidate;
 
     return (
-            <div>
-                <h1>Candidate Info</h1>
-                <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
-                    <p><strong>Name:</strong> {candidate.firstName} {candidate.lastName}</p>
-                    <p><strong>Phone:</strong> {candidate.phoneNumber}</p>
-                    <p><strong>Email:</strong> {candidate.email}</p>
-                    <p><strong>Job Title:</strong> {candidate.job.jobTitle}</p>
-                </div>
+        <div>
+            <h1>Candidate Info</h1>
+            <div style={{ border: '1px solid black', padding: '10px', margin: '10px' }}>
+                <p><strong>Name:</strong> {candidate.firstName} {candidate.lastName}</p>
+                <p><strong>Phone:</strong> {candidate.phoneNumber}</p>
+                <p><strong>Email:</strong> {candidate.email}</p>
+                <p><strong>Job Title:</strong> {candidate.job.jobTitle}</p>
             </div>
-        );
+        </div>
+    );
 };
 
 export default SingleApplication;
