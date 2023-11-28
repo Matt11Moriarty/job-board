f433d1ee0027f269a613e25925bdcceec01370dd
PeterJanasz <petertjanasz@gmail.com>
2023-11-27 21:26:46 -0700

    fixed apllication succesful alert.

--- client/src/components/ApplyForm/index.jsx
+++ client/src/components/ApplyForm/index.jsx
@@ -9,13 +9,13 @@ const ApplyForm = ({ job }) => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
   const [createCandidate, { error }] = useMutation(CREATE_CANDIDATE);
-
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
       const data = await createCandidate({
-        variables: {
+         variables: {
           firstName,
           lastName,
           phoneNumber,
@@ -25,44 +25,39 @@ const ApplyForm = ({ job }) => {
             salary: job.salary,
             jobDescription: job.jobDescription
           }
-        }
+         }
       })
       setFirstName('');
       setLastName('');
       setPhoneNumber('');
       setEmail('');
-
-      setMessage('Application submitted successfully!');
+      alert("Application sent successfully!");
     } catch (err) {
       console.error(err);
-      setMessage('Error submitting application. Please try again.');
     }
 
   };
 
   return (
-    <div>
-      {message && <p className={error ? 'error' : 'success'}>{message}</p>}
-      <form className="applyForm" onSubmit={handleSubmit}>
-        <label>
-          First Name:
-        </label>
-        <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
-        <label>
-          Last Name:
-        </label>
-        <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
-        <label>
-          Phone:
-        </label>
-        <input type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
-        <label>
-          Email:
-        </label>
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-        <input onClick={createCandidate} type="submit" value="Apply" />
-      </form>
-    </div>
+    <form className="applyForm" onSubmit={handleSubmit}>
+      <label>
+        First Name:
+      </label>
+      <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
+      <label>
+        Last Name:
+      </label>  
+      <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
+      <label>
+        Phone:
+      </label> 
+      <input type="tel" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
+      <label>
+        Email:
+      </label>  
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input onClick={createCandidate} type="submit" value="Apply" />
+    </form>
   );
 };
 
